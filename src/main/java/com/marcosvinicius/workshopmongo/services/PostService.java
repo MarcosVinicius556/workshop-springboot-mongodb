ca7daa0cb8df4463d52a72761bd321f52d2fc4c9
Marcos Vinicius Angeli Costa <71727165+MarcosVinicius556@users.noreply.github.com>
2023-03-19 12:41:18 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcosvinicius/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.marcosvinicius.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosvinicius.workshopmongo.domain.Post;
+import com.marcosvinicius.workshopmongo.repository.PostRepository;
+import com.marcosvinicius.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		if(!post.isPresent()) {
+			throw new ObjectNotFoundException("");
+		}
+		return post.get();
+	}
+	
+}
