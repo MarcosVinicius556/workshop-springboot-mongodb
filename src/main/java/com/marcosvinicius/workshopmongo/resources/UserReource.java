89c3a052ef8493ef503a35ff298ef0be4f05ee09
Marcos Vinicius Angeli Costa <71727165+MarcosVinicius556@users.noreply.github.com>
2023-03-18 23:29:26 -0300

    Conectando ao mongoDB e service

--- src/main/java/com/marcosvinicius/workshopmongo/resources/UserReource.java
+++ src/main/java/com/marcosvinicius/workshopmongo/resources/UserReource.java
@@ -2,21 +2,25 @@ package com.marcosvinicius.workshopmongo.resources;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcosvinicius.workshopmongo.domain.User;
+import com.marcosvinicius.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserReource {
 
+	@Autowired
+	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET) //Or @GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		return ResponseEntity.ok().body(null);
+		return ResponseEntity.ok().body(service.findAll());
 	}
 	
 }
