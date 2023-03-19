30fcf7ceb91d523fea9c566ff95fb6d8c99dfc9c
Marcos Vinicius Angeli Costa <71727165+MarcosVinicius556@users.noreply.github.com>
2023-03-19 00:45:11 -0300

    Obtendo um usuário por id e tratando exceção

--- src/main/java/com/marcosvinicius/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosvinicius/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.marcosvinicius.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcosvinicius.workshopmongo.domain.User;
 import com.marcosvinicius.workshopmongo.repository.UserRepository;
+import com.marcosvinicius.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		if(!user.isPresent()) {
+			throw new ObjectNotFoundException("");
+		}
+		return user.get();
 	}
 }
