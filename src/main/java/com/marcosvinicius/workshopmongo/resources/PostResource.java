86114e5bcc0ca6c450beaa9b5cbcfb6455fe10b5
Marcos Vinicius Angeli Costa <71727165+MarcosVinicius556@users.noreply.github.com>
2023-03-19 18:42:13 -0300

    Consulta simples com query methods

--- src/main/java/com/marcosvinicius/workshopmongo/resources/PostResource.java
+++ src/main/java/com/marcosvinicius/workshopmongo/resources/PostResource.java
@@ -1,18 +1,22 @@
 package com.marcosvinicius.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcosvinicius.workshopmongo.domain.Post;
+import com.marcosvinicius.workshopmongo.resources.util.URL;
 import com.marcosvinicius.workshopmongo.services.PostService;
 
 @RestController
 @RequestMapping(value="/posts")
-public class PostReource {
+public class PostResource {
 
 	@Autowired
 	private PostService service;
@@ -21,6 +25,15 @@ public class PostReource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET) //Or @GetMapping
+	//@RequestParam pega um parâmetro na requisição, sua anotação 
+	// " value="?" " indicia o parâmetro que ele irá pegar
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
